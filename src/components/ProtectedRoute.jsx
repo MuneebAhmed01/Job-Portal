ad286c87a26ec38bcda8aec46492c1a16395293c
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-08-25 23:19:56 +0500

    protected_router changes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,25 @@
-/* eslint-disable react/prop-types */
 import { Navigate, useLocation } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 
 const ProtectedRoute = ({ children }) => {
   const { isSignedIn, isLoaded, user } = useUser();
-  const { pathname } = useLocation();
+  const location = useLocation();
 
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
-    return <Navigate to="/?sign-in=true" />;
+  if (!isLoaded) return null; // or a spinner
+
+  if (!isSignedIn) {
+    return (
+      <Navigate
+        to="/sign-in"
+        replace
+        state={{ from: location.pathname }}
+      />
+    );
   }
 
-  if (
-    user !== undefined &&
-    !user?.unsafeMetadata?.role &&
-    pathname !== "/onboarding"
-  )
-    return <Navigate to="/onboarding" />;
+  if (!user?.unsafeMetadata?.role && location.pathname !== "/onboarding") {
+    return <Navigate to="/onboarding" replace />;
+  }
 
   return children;
 };
