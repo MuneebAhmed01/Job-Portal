fd82afe2e66696b8c05c0f86546cbf2fb08ca799
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-08-25 23:19:41 +0500

    header_commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   SignedIn,
   SignedOut,
   UserButton,
-  SignIn,
   useUser,
 } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
-  const [showSignIn, setShowSignIn] = useState(false);
-
-  const [search, setSearch] = useSearchParams();
   const { user } = useUser();
-
-  useEffect(() => {
-    if (search.get("sign-in")) {
-      setShowSignIn(true);
-    }
-  }, [search]);
-
-  const handleOverlayClick = (e) => {
-    if (e.target === e.currentTarget) {
-      setShowSignIn(false);
-      setSearch({});
-    }
-  };
 
   return (
     <>
@@ -38,10 +20,12 @@ const Header = () => {
 
         <div className="flex gap-8">
           <SignedOut>
-            <Button variant="outline" onClick={() => setShowSignIn(true)}>
-              Login
-            </Button>
+            {/* Redirect to sign-in page instead of overlay */}
+            <Link to="/sign-in">
+              <Button variant="outline">Login</Button>
+            </Link>
           </SignedOut>
+
           <SignedIn>
             {user?.unsafeMetadata?.role === "recruiter" && (
               <Link to="/post-job">
@@ -51,6 +35,7 @@ const Header = () => {
                 </Button>
               </Link>
             )}
+
             <UserButton
               appearance={{
                 elements: {
@@ -75,18 +60,6 @@ const Header = () => {
           </SignedIn>
         </div>
       </nav>
-
-      {showSignIn && (
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-          onClick={handleOverlayClick}
-        >
-          <SignIn
-            signUpForceRedirectUrl="/onboarding"
-            fallbackRedirectUrl="/onboarding"
-          />
-        </div>
-      )}
     </>
   );
 };
