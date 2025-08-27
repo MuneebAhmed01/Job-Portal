cd51e9d6b4a7a4a5087230fd9457a9a172503d18
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-08-27 20:39:45 +0500

    name change in supabase

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,20 +1,18 @@
+// src/utils/supabase.js
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseClient = async(supabaseAcessToken)=>{
-    const supabase = createClient(supabaseUrl, supabaseKey,{
-        global:{
-            headers:{
-                Authorization:`Bearer ${supabaseAcessToken} `
-            }
-        }
+export const supabaseClient = async (supabaseAccessToken) => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        Authorization: `Bearer ${supabaseAccessToken}`,
+      },
+    },
+  });
+  return supabase;
+};
 
-    });
-    return supabase;
-    
-}
-export default supabase
-        
+export default supabaseClient;
